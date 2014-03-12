f56b1ed19fbfe3bb26479771cc761356192bf5c4
Nejc Haberman <nejc.haberman@gmail.com>
2014-03-12 17:35:06 +0000

    added branch point candidates for SAM file

--- get_branch_point_candidates.py
+++ get_branch_point_candidates.py
@@ -3,32 +3,38 @@ Created on Jan 8, 2014
 
 @author: Nejc Haberman
 
-The script will filter out only fasta sequences that ends with AG
+The script will filter out only sequences that ends with AG
 '''
 
 
 import sys
 
-def filter_branch_point_seq (fin_fasta, fout_fasta):
-    finFasta = open(fin_fasta, "rt")
-    foutFasta = open(fout_fasta, "w")
-    line = finFasta.readline()
+def filter_branch_point_seq (fin_sam, fout_sam):
+    fin = open(fin_sam, "rt")
+    fout = open(fout_sam, "w")
+    line = fin.readline()
+    while line[0] == '@':    #header of .SAM
+        fout.write(line)
+        line = fin.readline()
+        
     while line:
-        if line[0] != '>':
-            line = line.rstrip('\n')
-            if str(line[-2:]).upper() == "AG":
-                foutFasta.write(id)
-                foutFasta.write(line + '\n')
-        else:
-            id = line
-        line = finFasta.readline()
-    finFasta.close()
-    foutFasta.close()
+        column = line.rstrip('\n').rstrip('\r').split('\t')
+        if column[1] != '4':    #not error 
+            seq = column[9]
+            if column[1] != '0':    #same strand
+                if str(seq[-2:]).upper() == "AG":
+                    fout.write(line)
+            if column[1] != '16':   #anti strand 
+                if str(seq[0:2]).upper() == "CT":
+                    fout.write(line)
+        line = fin.readline()
+    fin.close()
+    fout.close()
     
 if sys.argv.__len__() == 3:
-    fin_fasta = sys.argv[1]
-    fout_fasta = sys.argv[2]
-    filter_branch_point_seq (fin_fasta, fout_fasta)
+    fin_sam = sys.argv[1]
+    fout_sam = sys.argv[2]
+    filter_branch_point_seq (fin_sam, fout_sam)
 else:
     print "you need 2 arguments to run the script"
     quit()
